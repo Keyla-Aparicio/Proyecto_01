c616db14f3b7409780d2a7164a85668a5d28013f
Keyla-Aparicio <keylaparicio18@gmail.com>
2025-04-01 14:01:37 -0700

    Agregué dos archivos

--- index.js
+++ index.js
@@ -1,117 +1,157 @@
+// const fechData = async(seahTerm)=> {
+//     const response = await axios.get('http://omdbapi.com/', {
+//         params:{
+//             apikey: '4bd2499f',
+//             s: 'avengers'
+//         }
+//     })
 
-const fetchData = async(searchTerm) =>{
-    //
- const response = await axios.get ('http://omdbapi.com', {
-    params: {
-        apikey:'edae8d4e ',
-        s:'avengers'
+// if(response.data.Error){
+//     return []
+// }
+// console.log(response.data)
+
+// }
+
+const fetchData = async (searchTerm) => {
+    const response = await axios.get('https://omdbapi.com/',{
+        params: {
+            apikey: "edae8d4e",
+            i: movie.imdbID
+        }
+    })
+    console.log(response.data)
+    summaryElement.innerHTML = movieTemplate(response.data)
+
+        //preguntamos cual lado es?
+    if(side === "left"){
+        Leftmovie = response.data
+
+    }else{
+        rightMovie = response.data
     }
- })
- if(response.data.Error){
-    return[]
- }
-    console.log(response.data.Search)
+    //preguntamos si tenemos ambos lados
+    if(Leftmovie && rightMovie){
+        //entonces ejecutamos la funcion de comparacion
+        runComparison()
+
+    }
 
 }
-//fetchData()
-//va agarrar el elemento del htlml
+
+const runComparison = () => {
+    console.log("comparacion de peliculas")
+    const leftSideStats = document.querySelector("left-summary . notification")
+    const rightSideStats = document.querySelector("rightft-summary . notification")
+    leftSideStats.foreach((leftStat, index) =>{
+        const rightStat = rightSideStats[index]
+        const leftSideValue = parseInt(leftStat.dataset.value)
+        const rightSideValue = parseInt(rightSideStats.dataset.value)
+        
+        if(rightSideValue > leftSideValue){
+            leftStat.classList.remove("is-primary")
+            leftStat.classList.add("is-danger")
+        }else{
+            rightStat.classList.remove("is-primary")
+            rightStat.classList.add("is-danger")
+
+        }
+    })
+}
+
+// fechData()
 const root = document.querySelector('.autocomplete')
-root.innerHTML = ` 
-    <label><b>Busqueda de Peliculas</b></label>
-    <input class= "input"/>
-    <div class= "dropdown">
-         <div class= "dropdown-menu"/>
-            <div class= "dropdown-content results"></div>
-         </div>
+root.innerHTML = `
+<label><b>Búsqueda de películas</b></label>
+<input class="input">
+<div class="dropdown">
+    <div class="dropdown"-menu>
+        <div class="dropdown-content results></div>
     </div>
+</div>
 `
+
 const input = document.querySelector("input")
 const dropdown = document.querySelector(".dropdown")
 const resultsWrapper = document.querySelector(".results")
 
-//esta funcion con una espera para trabajar con la consulta
-const debonce = (func, dealay = 1000)=> {
-    return(...args)=> {
+const debonce = (func, delay = 1000) => {
+    let timeoutId 
+    return(...args) => {
         clearTimeout(timeoutId)
         timeoutId = setTimeout(() => {
             func.apply(null, args)
-        },dealay)
+        }, delay)
     }
 }
 
-//cuando teclee ocurrira un evento por medio de la variable movie 
 const onInput = async(event) => {
-    const movies = await fetchData(event.taget.value)
-    console.log("Movies ", movies )
+    const movies = await fechData(event,target,value)
+    console.log("MOVIES", movies)
 
-    if(!movies.length){
+    if(movies.length) {
         dropdown.classList.remove('is-active')
-        return  
+        return
     }
 
     resultsWrapper.innerHTML = ''
     dropdown.classList.add('is-active')
 
-    for(let movie of movies){
+    for(let movie of movies) {
         const option = document.createElement('a')
-        const imgSrc= movie.Poster === 'N/A' ?  '': movie.Poster //si hay un poster entonces traer la imagen si no, no existe el poster 
-
-        option.classList.add('')
-
-
+        const imgSrc = movie.Poster === 'N/A' ? '': movie.Poster
 
         option.classList.add('dropdown-item')
-        option.innerHTML = `  
-            <img src="${imgSrc}" />
-            ${movie.Title}
-
+        option.innerHTML = `
+        <img src="${imgSrc}" />
+        ${movie.Title}
         `
-        //cuandole declick va remover el nombre de is-active 
         option.addEventListener('click', () => {
             dropdown.classList.remove('is-active')
             input.value = movie.Title
             onMovieSelect(movie)
-
         })
         resultsWrapper.appendChild(option)
     }
 }
-//cuando tecleemmos empazara agregar clases 
- input.addEventListener('input', debonce(onInput,1000))
 
- document.addEventListener('click', event =>{
-    if(!root.contains(event.target )){
+input.addEventListener('input', debonce(onInput, 1000))
+
+document.addEventListener('click', event => {
+    if(!root.contains(event.target)){
         dropdown.classList.remove('is-active')
     }
- })
+})
 
- const onMovieSelect = async(movie) =>{
-    const response = await axios.get('http:www.omdbapi.com/', {
-        params:{
-            apikey: '',
-            i: movie.imdbID
+const onMovieSelect = async (movie) => {
+    const response = await axios.get('http://www.omdbapi.com/',{
+        params: {
+            apikey: 'edae8d4e',
+            i: movie.imdID
         }
     })
-    console.log(response.data)
- document.querySelector('#summary').innerHTML = movieTemplate(response.data)
- }
 
- const movieTemplate = (movieDetail) => {
-    return
-    <article class="media">
-        <figure class="media-left">
-            <p class="image">
-                <img src="${movieDetail.Poster}"></img>
-            </p>
-        </figure>
-        <div class="media-content">
-             <div class="content">
-                <h1>${movieDetail.Title}</h1>
-                <h1>${movieDetail.Genre}</h1>
-                <p>${movieDetail.Plot}</p>
+
+console.log(response.data)
+document.querySelector('#sumary').innerHTML = movieTemplate()
+
+}
+
+const movieTemplate = (movieDetail) => {
+    return `
+        <article class="media"> 
+            <figure class="media-left">
+                <p class="image">
+                    <img src="${movieDetail.Poster}" />
+                </p
+            </figure>
+            <div class="media-content">
+                <div class="content">
+                    <h1>${movieDetail.Title}</h1>
+                    <h4>${movieDetail.Genre}</h4>
+                    <p>${movieDetail.Plot}</p>
+                </div>
             </div>
-         </div>
-    </article>
- }
-
- 
+        </article>        
+    `
+}
